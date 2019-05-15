8b9bcb4406415303dd0c80b562db97039de89673
Greg Rotter <grotter@gmail.com>
2019-05-15 10:15:10 -0700

    brand evo updates

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 aws s3 sync --region us-west-2 "`dirname "$0"`" s3://wifi.calacademy.org --exclude=".*/*" --exclude="*.DS_Store" --delete
 
 # cloudfront invalidate
-aws cloudfront create-invalidation --region us-west-2 --distribution-id EQK4084546DXW --paths /index.html
+aws cloudfront create-invalidation --region us-west-2 --distribution-id EQK4084546DXW --paths /\*
